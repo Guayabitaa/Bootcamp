dc9ab9f2edc1ad8712e0c36e52b4c482ad06e8d2
user <guayabitacorreo@gmail.com>
2022-03-12 12:24:25 -0600

    main

--- Bootcamp/script.js
+++ Bootcamp/script.js
@@ -1,1 +1,14 @@
-console.log('Yang')
+const list = []
+const anotherList = list.concat(27)
+
+const persona = {
+    name: "Yang",
+    twitter: "@Yang",
+    age: "1",
+    isDeveloper: true,
+    links: ['https://pandacommunity.org', 'https://tuliotriste.me']
+}
+
+const field = 'twitter'
+
+console.log(persona['twitter'])
